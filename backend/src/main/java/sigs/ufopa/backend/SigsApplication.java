a8c4b4ea2ecb2fdbb48ff72afc0de95123706721
elian-sss <elian.silva.if@gmail.com>
2025-05-20 10:48:04 -0300

    first commit

---
+++ backend/src/main/java/sigs/ufopa/backend/SigsApplication.java
@@ -0,0 +1,13 @@
+package sigs.ufopa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sigs/ufopa/backend/SigsApplicationTests.java
@@ -0,0 +1,13 @@
+package sigs.ufopa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

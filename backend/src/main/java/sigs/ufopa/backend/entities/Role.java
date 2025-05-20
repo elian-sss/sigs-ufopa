4acf846b9ba788dfa1c50bf351fd2b8ca48766c0
elian-sss <elian.silva.if@gmail.com>
2025-05-20 11:59:22 -0300

    add relation user and roles and enum values

--- backend/src/main/java/sigs/ufopa/backend/entities/Role.java
+++ backend/src/main/java/sigs/ufopa/backend/entities/Role.java
@@ -10,4 +10,41 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+        ADMIN(1L),
+        COORDENADOR(2L),
+        SEGURANCA(3L),
+        USUARIO(4L);
+
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+
+        public void setRoleId(long roleId) {
+            this.roleId = roleId;
+        }
+    }
 }
--- backend/src/main/java/sigs/ufopa/backend/entities/User.java
+++ backend/src/main/java/sigs/ufopa/backend/entities/User.java
@@ -21,6 +21,59 @@ public class User {
     private String email;
     private String password;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id")
+    )
     private Set<Role> roles;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
